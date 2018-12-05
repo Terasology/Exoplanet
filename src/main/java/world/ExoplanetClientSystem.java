556d8cf6c70f07b5285b1a81917353506613e855
PutawanDE <putawan-de@hotmail.co.th>
2018-12-05 20:28:46 +0700

    Add tree, grass, dirt blocks and event system

---
+++ src/main/java/world/ExoplanetClientSystem.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package world;
+
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.logic.characters.CharacterTeleportEvent;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.registry.In;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Vector;
+
+@RegisterSystem(RegisterMode.CLIENT)
+public class ExoplanetClientSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
+
+    private Map<EntityRef, Vector3f> teleportQueue = new HashMap<>();
+
+    @Override
+    public void update(float delta) {
+        if ((!teleportQueue.isEmpty())) {
+            Iterator<Map.Entry<EntityRef, Vector3f>> teleportIt = teleportQueue.entrySet().iterator();
+            while (teleportIt.hasNext()) {
+                Map.Entry<EntityRef, Vector3f> entry = teleportIt.next();
+                EntityRef character = entry.getKey();
+                Vector3f targetPos = entry.getValue();
+                character.send(new CharacterTeleportEvent(targetPos));
+                teleportIt.remove();
+            }
+        }
+    }
+
+    @ReceiveEvent
+    public void onEnterExoplanet (EnterExoplanetEvent event, EntityRef entity) {
+        //TODO: Play Teleporting Sound
+    }
+}
