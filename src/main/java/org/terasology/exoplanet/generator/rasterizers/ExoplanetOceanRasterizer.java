689eef14b0001423239e86e029c9bf062233fe10
Michael Pollind <mpollind@gmail.com>
2021-01-16 19:31:22 -0800

    feat(JOML): migrate ExoplanetOceanRasterizer

--- src/main/java/org/terasology/exoplanet/generator/rasterizers/ExoplanetOceanRasterizer.java
+++ src/main/java/org/terasology/exoplanet/generator/rasterizers/ExoplanetOceanRasterizer.java
@@ -15,11 +15,11 @@
  */
 package org.terasology.exoplanet.generator.rasterizers;
 
-import org.terasology.math.geom.Vector3i;
+import org.joml.Vector3ic;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockManager;
-import org.terasology.world.chunks.ChunkConstants;
+import org.terasology.world.chunks.Chunks;
 import org.terasology.world.chunks.CoreChunk;
 import org.terasology.world.generation.Region;
 import org.terasology.world.generation.WorldRasterizer;
@@ -34,7 +34,7 @@ public class ExoplanetOceanRasterizer implements WorldRasterizer {
 
     @Override
     public void generateChunk(CoreChunk chunk, Region chunkRegion) {
-        for (Vector3i pos : ChunkConstants.CHUNK_REGION) {
+        for (Vector3ic pos : Chunks.CHUNK_REGION) {
             chunk.setBlock(pos, water);
         }
     }
