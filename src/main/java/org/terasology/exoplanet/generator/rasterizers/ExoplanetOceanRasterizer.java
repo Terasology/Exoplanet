e37e03227d0d88bdfd2beff0e26745daaa34fa98
jdrueckert <jd.rueckert@googlemail.com>
2021-01-29 22:44:50 +0100

    feat(JOML): migrate Exoplanet (#19)
    * chore: remove unused imports
    * chore: update copyright

--- src/main/java/org/terasology/exoplanet/generator/rasterizers/ExoplanetOceanRasterizer.java
+++ src/main/java/org/terasology/exoplanet/generator/rasterizers/ExoplanetOceanRasterizer.java
@@ -1,25 +1,12 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.exoplanet.generator.rasterizers;
 
-import org.terasology.math.geom.Vector3i;
+import org.joml.Vector3ic;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockManager;
-import org.terasology.world.chunks.ChunkConstants;
+import org.terasology.world.chunks.Chunks;
 import org.terasology.world.chunks.CoreChunk;
 import org.terasology.world.generation.Region;
 import org.terasology.world.generation.WorldRasterizer;
@@ -34,7 +21,7 @@ public class ExoplanetOceanRasterizer implements WorldRasterizer {
 
     @Override
     public void generateChunk(CoreChunk chunk, Region chunkRegion) {
-        for (Vector3i pos : ChunkConstants.CHUNK_REGION) {
+        for (Vector3ic pos : Chunks.CHUNK_REGION) {
             chunk.setBlock(pos, water);
         }
     }
