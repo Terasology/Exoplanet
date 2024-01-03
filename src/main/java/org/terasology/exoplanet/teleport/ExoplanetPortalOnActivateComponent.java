126c8a37e05e665c2436233d39de41b7e213905d
Zangus <wong.thi@gmail.com>
2024-01-03 19:27:46 +0100

    refactor: Components without attributes to EmptyComponent (#26)
    Co-authored-by: thierrywong <wong.t@sfeir.com>

--- src/main/java/org/terasology/exoplanet/teleport/ExoplanetPortalOnActivateComponent.java
+++ src/main/java/org/terasology/exoplanet/teleport/ExoplanetPortalOnActivateComponent.java
@@ -2,11 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.exoplanet.teleport;
 
-import org.terasology.gestalt.entitysystem.component.Component;
+import org.terasology.gestalt.entitysystem.component.EmptyComponent;
 
-public class ExoplanetPortalOnActivateComponent implements Component<ExoplanetPortalOnActivateComponent> {
-    @Override
-    public void copyFrom(ExoplanetPortalOnActivateComponent other) {
-
-    }
+public class ExoplanetPortalOnActivateComponent extends EmptyComponent<ExoplanetPortalOnActivateComponent> {
 }
